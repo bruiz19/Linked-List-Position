6bd0ece439a042dba68a86bbf2948f8c64dadca5
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-07 16:01:59 -0700

    Create IntListNode.h

---
+++ IntListNode.h
@@ -0,0 +1,15 @@
+// Solution to sample test provided by Chary Vielma
+#pragma once
+
+class IntListNode {    // singly linked list node
+    public:
+        IntListNode(int e, IntListNode *nextptr): 
+            elem(e), next(nextptr) { }
+        int &getElement() {return elem;}
+        IntListNode *getNext() {return next;}
+        void setNext(IntListNode* n) {next = n;}
+        
+    private:
+        int elem;    // linked list element value
+        IntListNode *next;    // next item in the list
+};
