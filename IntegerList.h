c23120406d81402bd0f659e89898b0c23442a051
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-07 16:01:08 -0700

    Create IntegerList.h

---
+++ IntegerList.h
@@ -0,0 +1,25 @@
+// Solution to sample test provided by Chary Vielma
+#pragma once
+
+#include <stdexcept>
+#include "IntListNode.h"
+
+using namespace std;
+
+class IntegerList {    // a singly linked list
+public:
+    IntegerList();    // empty list constructor
+    ~IntegerList();    // destructor
+    int& front();    // return front element
+    void addFront(int e);    // add to front of list
+    void removeFront();   // remove front item list
+    void print();
+    int size() const;   // list size
+    void AddAtPosition(int e, int p);
+    int  sum();   // returns the sum of the nodes in the list
+    int  sum(IntListNode* node);   // recursive function, used by above function
+
+private:
+    IntListNode* head;   // head of the list
+    int     n;   // number of items
+};
