3269806b6080ca0b0bbbe5774a656cc147af7a33
bruiz19 <31776936+bruiz19@users.noreply.github.com>
2019-05-08 10:32:10 -0700

    Update IntegerList.h

--- IntegerList.h
+++ IntegerList.h
@@ -14,11 +14,12 @@ public:
     void addFront(int e);    // add to front of list
     void removeFront();   // remove front item list
     void print();
-    int size() const;   // list size
+    int size() const;   // list size    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void AddAtPosition(int e, int p);
     int  sum();   // returns the sum of the nodes in the list
     int  sum(IntListNode* node);   // recursive function, used by above function
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 private:
     IntListNode* head;   // head of the list
     int     n;   // number of items
